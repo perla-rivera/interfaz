77dbf9585f77e9997bca202d542e20eb89c6ab5c
ambaratzin <ambar.atzin.gv@gmail.com>
2016-08-29 00:02:47 +0000

    Primer programa por Euribe

---
+++ aEspinoza/VictorCorrea/euclid.s
@@ -0,0 +1,21 @@
+.section .data
+vals:
+    .long 6099, 2166
+.section .text
+.globl   _start
+
+_start:
+    ldr r2,  =vals
+    ldr r1,  [r2]
+    ldr r0,  [r2, #4]
+
+gdc:
+    cmp r0, r1
+    subgt r0, r0, r1
+    sublt r1, r1, r0
+    
+
+    mov r7, $1
+    swi 0
+
+  .end
