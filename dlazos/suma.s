865ec57320817cc2eefbc60d0d445dd051d13866
danielopez25 <isc.edgar.alvarado@gmail.com>
2016-09-01 16:15:36 +0000

    Practica 2

---
+++ dlazos/suma.s
@@ -0,0 +1,11 @@
+.section    .data   	@ a section for data (unused)
+.section    .text   	@ a section for text (unused)
+.globl      _start  	@ sets the global label "_start"
+
+_start:             	@ code starts here.
+mov %r1, $7         	@ puts the value 7 in r1
+mov %r0, $8		@ puts the value 8 in r0
+add %r0, %r0, %r1	@ add r0 to r1 and store result in r0
+
+mov %r7, $1         	@ this is how to set an exit syscall
+svc $0
