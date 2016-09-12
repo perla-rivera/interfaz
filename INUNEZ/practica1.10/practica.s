2db72cff6e6ee5bef7cf144521ddcc398210ae9c
irnh99 <ivan.nunez@tectijuana.edu.mx>
2016-09-12 01:24:55 +0000

    practica10

--- INUNEZ/practica1.10/practica.s
+++ INUNEZ/practica1.10/practica.s
@@ -2,23 +2,31 @@
 .section	.text
 
 numbers:
-.long 3,4,9,7,5,75,2,5,4,3,6,4,367,7,87,64,3,1,4,7,8,7,93,64,978
+.long 3,4,9,7,5,7,2,5,4,3,6,4,6,7,8,6,3,1,4,7,8,7,9,6,9,0
+numbers2:
+.long 0
 
 .globl		_start
 _start:
-ldr %r1 =numbers
-ldr %3, [%r1]
-mov %r0, %r3
+ldr %r1, =numbers
+ldr %r3, [%r1]
+ldr %r0, =numbers2
 
 loop:
 	cmp %r3, $0
 	beq exit
-	ldr%r3,[%r1,#4]!
 
-	
+	and %r3, $1 @ check even or odd
+	mov %r3, $2 @par
+	ble next
+	mov %r3, $1 @impar
 
+next:
+	str %r0, [%r3] @almacenar resultado
+	ldr %r3,[%r1,#4]!
+	ble loop
 exit:
 
-mov r7, $1	@ exit syscall
+mov %r7, $1	@ exit syscall
 svc $0		@ wake kernel
 .end
