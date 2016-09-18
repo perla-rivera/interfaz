0ce603fbeae6f1be1bbc3b289c4a003c9d69e834
alejandro-espinoza <alesscovarrubias02@gmail.com>
2016-09-18 09:15:27 +0000

    Practica 7

--- aEspinoza/Alexeuclid.s
+++ aEspinoza/Alexeuclid.s
@@ -2,10 +2,10 @@
 @ Practica 5
 
 @ euclid.s
-@ GCD algorithm (example taken from Knuth TAOCP, p.9)
+@ Algoritmo GCD (ejemplo tomado de  Knuth TAOCP, p.9)
 
-@ r1 holds the firts value
-@ r0 holds the second
+@ r1 tiene el primer valor
+@ r0 tiene el sgvundo valor
 
 .section	.data
 vals:
@@ -14,17 +14,17 @@ vals:
 .globl	_start
 
 _start:
-	ldr r2, =vals		@ make r1 point to start of vals
-	ldr r1, [r2]		@ load first number into r2
-	ldr r0, [r2, #4]	@ load second number into r0
+	ldr r2, =vals		@ Hace a r1 punto de partido de los valores
+	ldr r1, [r2]		@ Carga el primer valor en r2
+	ldr r0, [r2, #4]	@ Carga el segundo valor en r0
 
 gcd:
-	cmp r0, r1		@ compare r0 and r1
-	subgt r0, r0, r1	@ if r0 > r1, r0 = r0 - r1
-	sublt r1, r1, r0	@ if r0 < r1, r1 = r1 - r0
-	bne gcd			@ if r0 != r1, repeat
+	cmp r0, r1		@ Compara r0 y r1
+	subgt r0, r0, r1	@ Si r0 > r1, r0 = r0 - r1
+	sublt r1, r1, r0	@ Si r0 < r1, r1 = r1 - r0
+	bne gcd			@ Si r0 != r1, repite
 
-	mov 	r7, $1		@ prepare to exit 
-	swi	0		@ wake kernel
+	mov 	r7, $1		@ Prepar para sali 
+	swi	0		@ Activar kernel
 
 .end
