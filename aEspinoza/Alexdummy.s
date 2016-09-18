4313376515b0a4ab10b73f2fba20b34540902bab
alejandro-espinoza <alesscovarrubias02@gmail.com>
2016-09-18 09:00:16 +0000

    Practica 7

--- aEspinoza/Alexdummy.s
+++ aEspinoza/Alexdummy.s
@@ -2,14 +2,14 @@
 @ Practica 7
 
 @ dummy.s
-@ a template for asm programs
+@ un template para programas ensamblador
 
 .section	.data
 .section	.text
 .globl		_start
 
 _start:
-nop		@ no operation
-mov r7, $1	@ exit syscall
-svc $0		@ wake kernel
+nop		@ sin operacion
+mov r7, $1	@ salida de  syscall
+svc $0		@ activar kernel
 .end
