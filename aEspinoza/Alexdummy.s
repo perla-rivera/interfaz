638151017558040aa94f6d996b244cd8cd30b9f0
alejandro-espinoza <alesscovarrubias02@gmail.com>
2016-09-18 21:43:58 +0000

    Practica 7

---
+++ aEspinoza/Alexdummy.s
@@ -0,0 +1,15 @@
+@ Espinoza Covarrubias Alejandro 13211465
+@ Practica 7
+
+@ dummy.s
+@ a template for asm programs
+
+.section	.data
+.section	.text
+.globl		_start
+
+_start:
+nop		@ no operation
+mov r7, $1	@ exit syscall
+svc $0		@ wake kernel
+.end
