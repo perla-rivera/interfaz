b3971a3f8aa9b677e51b342ebdb411b7635687e7
Miguel Lapizco <raexe@outlook.com>
2016-09-27 17:15:37 -0700

    Agregar comentarios

--- mlapizco/Raspian Assembler: Problema 19/borrador.s
+++ mlapizco/Raspian Assembler: Problema 19/borrador.s
@@ -1,15 +1,22 @@
-        .global _start
+@ Álvarez Lapizco Miguel Ángel
+@ Lenguajes de Interfaz
 
+.global _start
+@ Etiqueta de comienzo.
 _start:
-        MOV R0, #0
-        MOV R2, #0
+        MOV R0, #0 @ Colocar el valor de 0 al registro 0.
+        MOV R2, #0 @ Colocar el valor de 0 al registro 2.
+        
+@ Ciclo para sumar los números.
 loop:
-        ADD R0, R0, #1
-        ADD R2, R2, R0
-        CMP R0, #15
-        BLE loop
+        ADD R0, R0, #1  @ Valor para la condición (i++).
+        ADD R2, R2, R0  @ Se suma el valor i a R2.
+        CMP R0, #15     @ Se compara i con el límite del ciclo.
+        BLE loop        @ Se repite el ciclo.
+        
+@ Salir del programa.
 _exit:
-        MOV R0, R2
+        MOV R0, R2      
         MOV R7, #1
         SWI 0
 
