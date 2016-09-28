b3dfe16cd2e193bfb97a04639f76feaf61ea266d
Miguel Lapizco <raexe@outlook.com>
2016-09-27 17:21:22 -0700

    Agregar comentarios.

--- mlapizco/asmtut/hello_world.s
+++ mlapizco/asmtut/hello_world.s
@@ -1,16 +1,22 @@
+@ Miguel Ángel Álvarez Lapizco
+@ Programa de Hola Mundo por ASMTut.
+
 .global _start
 
+@ Etiqueta de comienzo.
 _start:
-	MOV R7, #4
-	MOV R0, #1
-	MOV R2, #12
-	LDR R1, =message
+	MOV R7, #4 	@ Colocar el valor 4 en el Registro 7.
+	MOV R0, #1 	@ Colocar el valor 1 en el Registro 0.
+	MOV R2, #12 	@ Colocar el número de caracteres en la cadena a mostrar.
+	LDR R1, =message	@ Nombre de la variable que contiene el mensaje.
 	SWI 0
 
+@ Salir del programa.
 end:
 	MOV R7, #1
-	SWI 0
+	SWI 0 		@ SWI 0 para darle el control a la terminal.
 
 .data
+	@ Desplegar el mensaje.
 	message:
 		.ascii "Hello World\n"
